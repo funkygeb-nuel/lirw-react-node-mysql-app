843354f366080ae1084ef28e5bda65d34e800e70
funkygeb-nuel <funkygeb@gmail.com>
2026-07-06 13:39:21 +0100

    Update DB config to point to RDS endpoint

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 cd /home/ubuntu/backend
 npm install
+
+# Create database and import schema
+mysql -h mydemo-project-db.cst4u40c2903.us-east-1.rds.amazonaws.com \
+  -u admin \
+  -pyour-actual-rds-password \
+  -e "CREATE DATABASE IF NOT EXISTS react_node_app;"
+
+mysql -h mydemo-project-db.cst4u40c2903.us-east-1.rds.amazonaws.com \
+  -u admin \
+  -pyour-actual-rds-password \
+  react_node_app < /home/ubuntu/backend/db.sql
